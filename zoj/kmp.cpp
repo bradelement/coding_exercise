8233202887b94bec61b3d3b25082ff7814532383
bradelement <bradelementzju@gmail.com>
2014-05-08 20:41:14 +0800

    update kmp

--- zoj/kmp.cpp
+++ zoj/kmp.cpp
@@ -7,10 +7,11 @@
 #include "cassert"
 using namespace std;
 
-const int MAX_LEN = 1000005;
-
-char A[MAX_LEN], B[MAX_LEN]; 
-int next[MAX_LEN];
+const int MAX_SLEN = 512001;
+int next[MAX_SLEN];
+int num[MAX_SLEN];
+char text[MAX_SLEN];
+char pat[257];
 
 void deal_next(char B[], int m) {
     next[1] = 0;
@@ -22,30 +23,37 @@ void deal_next(char B[], int m) {
     }
 }
 
-/*
-bool kmp(char A[], int n, char B[], int m) {
-    deal_next(B, m);
 
+int kmp(char A[], int n, char B[], int m) {
+    int ans = 0;
+
+    deal_next(B, m);
     int j = 0;
+
+    stack<int> si;
     for (int i=1; i<=n; i++) {
+        si.push(i);
         while (j>0 && B[j+1]!=A[i]) j = next[j];
         if (B[j+1] == A[i]) j++;
+        num[i] = j;
         if (j == m) {
-            return true;
+            ans++;
+            for (int i=0; i<m; i++) si.pop();
+            if (si.empty()) j = 0;
+            else j = num[si.top()];
+            //return true;
             //j = next[j]; //continue to match
         }
     }
-    return false;
+    return ans;
 }
-*/
+
 int main(int argc, char const *argv[])
 {
-    while (~scanf("%s", A+1)) {
-        if (strcmp(A+1, ".") == 0) break;
-        int len = strlen(A+1);
-        deal_next(A, len);
-        if (len % (len - next[len]) == 0) printf("%d\n", len / (len - next[len]));
-        else printf("1\n");
+    while (~scanf("%s%s", pat+1, text+1)) {
+        int pLen = strlen(pat+1);
+        int tLen = strlen(text+1);
+        printf("%d\n", kmp(text, tLen, pat, pLen));
     }
 
     return 0;
