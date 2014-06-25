f283920dffebaa0d70dba6b863bf2c30760947a9
Chi Zhang <zhangchi@ucfgroup.com>
2014-06-25 21:29:55 +0800

    change tab to space

--- zoj/1729/1729.cpp
+++ zoj/1729/1729.cpp
@@ -5,32 +5,32 @@ const int MAXN = 100001;
 char s[MAXN];
 
 int minP(char *in, int len) {
-	int i=0, j=1, k=0, t;
-	while (i<len && j<len && k<len) {
-		t = s[(i+k) >= len ? i+k-len : i+k] - \
-			s[(j+k) >= len ? j+k-len : j+k];
-			
-		if (t == 0) k++;
-		else {
-			if (t>0) i += k+1;
-			else j += k+1;
-			if (i == j) j++;
-			k = 0;
-		}
-	}
-	
-	return i<j ? i:j;
-	
+    int i=0, j=1, k=0, t;
+    while (i<len && j<len && k<len) {
+        t = s[(i+k) >= len ? i+k-len : i+k] - \
+            s[(j+k) >= len ? j+k-len : j+k];
+            
+        if (t == 0) k++;
+        else {
+            if (t>0) i += k+1;
+            else j += k+1;
+            if (i == j) j++;
+            k = 0;
+        }
+    }
+    
+    return i<j ? i:j;
+    
 }
 
 int main()
 {
-	int n, len;
-	scanf("%d", &n);
-	while (n--) {
-		scanf("%d%s", &len, s);
-		printf("%d\n", minP(s, len));
-	}
-	
-	return 0;
+    int n, len;
+    scanf("%d", &n);
+    while (n--) {
+        scanf("%d%s", &len, s);
+        printf("%d\n", minP(s, len));
+    }
+    
+    return 0;
 }
--- zoj/1729/2006.cpp
+++ zoj/1729/2006.cpp
@@ -5,33 +5,33 @@ const int MAXN = 10001;
 char s[MAXN];
 
 int minP(char *in, int len) {
-	int i=0, j=1, k=0, t;
-	while (i<len && j<len && k<len) {
-		t = s[(i+k) >= len ? i+k-len : i+k] - \
-			s[(j+k) >= len ? j+k-len : j+k];
-			
-		if (t == 0) k++;
-		else {
-			if (t>0) i += k+1;
-			else j += k+1;
-			if (i == j) j++;
-			k = 0;
-		}
-	}
-	
-	return i<j ? i:j;
-	
+    int i=0, j=1, k=0, t;
+    while (i<len && j<len && k<len) {
+        t = s[(i+k) >= len ? i+k-len : i+k] - \
+            s[(j+k) >= len ? j+k-len : j+k];
+            
+        if (t == 0) k++;
+        else {
+            if (t>0) i += k+1;
+            else j += k+1;
+            if (i == j) j++;
+            k = 0;
+        }
+    }
+    
+    return i<j ? i:j;
+    
 }
 
 int main()
 {
-	int n, len;
-	scanf("%d", &n);
-	while (n--) {
-		scanf("%s", s);
-		len = strlen(s);
-		printf("%d\n", minP(s, len)+1);
-	}
-	
-	return 0;
+    int n, len;
+    scanf("%d", &n);
+    while (n--) {
+        scanf("%s", s);
+        len = strlen(s);
+        printf("%d\n", minP(s, len)+1);
+    }
+    
+    return 0;
 }
