0083702472edeee68f030d32a73e6e0cde117bc4
bradelement <bradelementzju@gmail.com>
2014-09-17 23:32:03 +0800

    ....................

---
+++ zoj/math/3793.cpp
@@ -0,0 +1,13 @@
+#include "cstdio"
+
+int main(int argc, char const *argv[])
+{
+    int n;
+    scanf("%d", &n);
+    int b, e;
+    while (n--) {
+        scanf("%d%d", &b, &e);
+        printf("1\n");
+    }
+    return 0;
+}
